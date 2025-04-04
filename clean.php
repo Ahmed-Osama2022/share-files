1d9c89c85826a167036685df4efe20d59be29f46
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2025-04-04 23:41:01 +0200

    Add the deletion method for (tmp) Dir; Add confirmation code to make confirmation before delete the files; add security code to make sure only the admin of the server (localhost) can access the clean page.

--- clean.php
+++ clean.php
@@ -6,6 +6,23 @@
  */
 include './helpers.php';
 
+/**
+ * Prevent other users on ther server from access this page;
+ */
+$client_ip = $_SERVER['REMOTE_ADDR'];
+
+// Debug: just to check IP during development
+// echo "Client IP: $client_ip";
+// die();
+
+if ($client_ip !== '127.0.0.1' && $client_ip !== '::1') {
+  // Request NOT from localhost
+  echo "<h1 style='text-align: center; padding: 30px'>
+    Access denied. Only localhost can see this page!
+  </h1>";
+  exit;
+}
+
 // Define the dirs 
 $dir1 = basePath('uploads');
 $dir2 = basePath('tmp');
@@ -27,15 +44,17 @@ $messages = [
   'FoundFiles_dir2' => ''
 ];
 
-
-// Ensure the tmp directory exists
+/**
+ * | ================================== |
+ *  Function for the "Upload DIR"
+ * | ================================== |
+ */
+// Ensure the directory exists
 if (!file_exists($dir1)) {
-  // echo "The Upload folder doesn't exists";
-  // $message_dir1 = "The Upload folder doesn't exists";
-  $messages['FoundUpload'] = "The Upload folder doesn't exists";
+  $messages['FoundUpload'] = "The (Upload) folder doesn't exists";
   $messages['Dir_style1'] = 'text-danger';
 } else {
-  $messages['FoundUpload'] = "Found the Upload Dir!";
+  $messages['FoundUpload'] = "Found the (Upload) Dir!";
   $messages['Dir_style1'] = 'text-success';
 
   // Get all files and directories inside the target directory
@@ -45,47 +64,66 @@ if (!file_exists($dir1)) {
   if (count($files1) >= 1) {
     $btn_status = '';
     $messages['File_style1'] = 'text-success';
-    $messages['FoundFiles_dir1'] = "Folder Uploads contains a " .  count($files1) . ' Files';
+    $messages['FoundFiles_dir1'] = "Folder (Uploads) contains a " .  count($files1) . ' Files';
   } else {
     $btn_status = 'disabled';
-    $messages['FoundFiles_dir1'] = "Folder Uploads Is empty, and no need to clean.";
+    $messages['FoundFiles_dir1'] = "Folder (Uploads) Is empty, and no need to clean.";
     $messages['File_style1'] = 'text-danger';
   }
 }
 
-// var_dump($files1); // TEST
+/**
+ * | ================================== |
+ *  Function for the "tmp DIR"
+ * | ================================== |
+ */
+// Ensure the directory exists
+if (!file_exists($dir2)) {
+  // echo "The Upload folder doesn't exists";
+  // $message_dir2 = "The Upload folder doesn't exists";
+  $messages['FoundtTmp'] = "The (tmp) folder doesn't exists (For zipped files)";
+  $messages['Dir_style2'] = 'text-danger';
+} else {
+  $messages['FoundtTmp'] = "Found the (tmp) Dir! (For zipped files)";
+  $messages['Dir_style2'] = 'text-success';
+
+  // Get all files and directories inside the target directory
+  $files2 = array_diff(scandir($dir2), array('.', '..'));
+
+  // Check if the Dir has files inside it
+  if (count($files2) >= 1) {
+    $btn_status = '';
+    $messages['File_style2'] = 'text-success';
+    $messages['FoundFiles_dir2'] = "Folder (tmp) contains a " .  count($files2) . ' Files';
+  } else {
+    // $btn_status = 'disabled'; BUG:
+    $messages['FoundFiles_dir2'] = "Folder (tmp) Is empty, and no need to clean.";
+    $messages['File_style2'] = 'text-danger';
+  }
+}
 
 /**
  * Function to delete all of the files
  */
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
-  // echo "Ahmed";
-
   // Loop through each file/directory
   foreach ($files1 as $file) {
     $filePath = $dir1 . DIRECTORY_SEPARATOR . $file;
-
-    // echo $filePath . '<br>';
     // If it's a file, delete it
     unlink($filePath);
+    // sleep(3);
   }
+
+  foreach ($files2 as $file) {
+    $filePath = $dir2 . DIRECTORY_SEPARATOR . $file;
+    unlink($filePath);
+  }
+  return redirect('clean.php');
 }
-
-
-
-
-// Check the second dir => "/tmp"
-// if (!file_exists($dir2)) {
-//   // echo "The tmp folder doesn't exists";
-//   $message_dir2 = "The tmp folder doesn't exists";
-// } else {
-//   $files2 = array_diff(scandir($dir2), array('.', '..'));
-// }
-// // $filePath = $dir2 . DIRECTORY_SEPARATOR . $file;
-
 
 ?>
 
+<!-- | =========== Front-End ============== | -->
 <?= loadPartial('head', ['title' => 'Clean Script']) ?>
 
 <?= loadPartial('navbar') ?>
@@ -108,17 +146,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
     <!-- |======================================== | -->
     <!-- | ============ 2- For the "tmp" Directory && Files ============ | -->
     <p class="text-center p-3  <?= $messages['Dir_style2'] ?> ">
-      <?= $messages['FoundTmp'] ?>
+      <?= $messages['FoundtTmp'] ?>
     </p>
 
     <p class="text-center p-3  <?= $messages['File_style2'] ?>">
       <?= $messages['FoundFiles_dir2'] ?>
     </p>
 
-    <form method="post">
+    <form method="post" id="delete-form">
       <button class="btn btn-danger " <?= $btn_status ?> name="submit" type="submit">Delete All the files</button>
+
     </form>
-
-
   </div>
+
+  <!-- For confirmation -->
+  <script>
+    const form = document.getElementById('delete-form');
+
+    form.addEventListener('submit', function(e) {
+      const result = confirm('Are you sure you want to delete all the files?');
+
+      if (!result) {
+        e.preventDefault(); // This now works perfectly
+      }
+    });
+  </script>
+
 </body>
+
+
+</html>
